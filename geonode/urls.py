f66e75c9718a390722c2ed46013ad6bf6f804b08
Ismail Sunni <imajimatika@gmail.com>
2016-05-12 01:13:04 +0700

    Add back geosafe-related file.

--- geonode/urls.py
+++ geonode/urls.py
@@ -28,6 +28,7 @@ from django.contrib import admin
 import geonode.proxy.urls
 
 from geonode.api.urls import api
+import geosafe.urls
 
 import autocomplete_light
 
@@ -100,6 +101,7 @@ urlpatterns = patterns('',
                        (r'^documents/', include('geonode.documents.urls')),
                        (r'^services/', include('geonode.services.urls')),
                        url(r'', include(api.urls)),
+                       url(r'', include(geosafe.urls, namespace='geosafe')),
                        )
 
 if "geonode.contrib.dynamic" in settings.INSTALLED_APPS:
