c8cf1d96a4e3e3b446216ed53956a3b319c88960
ann26 <anita@kartoza.com>
2020-05-20 16:59:00 +0700

    add new landing page

--- igrac/urls.py
+++ igrac/urls.py
@@ -2,7 +2,10 @@
 """Urls for igrac apps."""
 
 from django.conf.urls import url
-
+from .views import HomeView
 
 urlpatterns = [
+    url(r'^$',
+        view=HomeView.as_view(),
+        name='home_igrac'),
 ]
