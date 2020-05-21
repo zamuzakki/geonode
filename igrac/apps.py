aee8188e07a817495b375699218005744db27aef
zamuzakki <age.zakki@gmail.com>
2020-05-21 14:39:35 +0700

    Move signals from models.py

--- igrac/apps.py
+++ igrac/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class IgracConfig(AppConfig):
     name = 'igrac'
+
+    def ready(self):
+        import igrac.signals
--- igrac/signals.py
+++ igrac/signals.py
@@ -1,14 +1,15 @@
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
 from geonode.groups.models import Group, GroupProfile
 
-
+@receiver(user_logged_in)
 def set_default_filter_by_group(sender, request, user, **kwargs):
     """
     Set layer/map/document to be filtered by user's group by default
     """
-
     groups = GroupProfile.objects.filter(
         group__in=request.user.groups.exclude(name='anonymous')
-    ).values_list('slug',flat = True)
+    ).values_list('slug', flat=True)
 
     filter = ""
     for group in groups:
