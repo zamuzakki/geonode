f68095e75dcf0550dd44299b0dc7270d88dfbbf6
zamuzakki <age.zakki@gmail.com>
2020-05-30 14:38:07 +0700

    Fix default filter problem when no group is assigned

--- igrac/context_processors.py
+++ igrac/context_processors.py
@@ -4,7 +4,9 @@ def resource_urls(request):
     """Global values to pass to templates"""
 
     defaults = dict(
-        DEFAULT_GROUP_FILTER=get_default_filter_by_group(request.user)
+        MAP_DEFAULT_FILTER=get_default_filter_by_group(request.user, type="map"),
+        LAYER_DEFAULT_FILTER=get_default_filter_by_group(request.user, type="layer"),
+        DOCUMENT_DEFAULT_FILTER=get_default_filter_by_group(request.user, type="document"),
     )
 
     return defaults
--- igrac/utilities.py
+++ igrac/utilities.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from geonode.groups.models import Group, GroupProfile
-
+from geonode.maps.models import Map
+from geonode.documents.models import Document
+from geonode.layers.models import Layer
 
 STOP_WORDS = (
     'a', 'an', 'and', 'if', 'is', 'the', 'in', 'i', 'you', 'other',
@@ -25,13 +27,28 @@ def check_slug(queryset, slug):
 
     return new_slug
 
-def get_default_filter_by_group(user):
+def get_default_filter_by_group(user, type='layer'):
     """
     Set layer/map/document filter by user's group by default
     """
-    groups = GroupProfile.objects.filter(
+    user_groups = GroupProfile.objects.filter(
         group__in=user.groups.exclude(name='anonymous')
-    ).values_list('slug', flat=True)
+    )
+    # values_list('slug', flat=True)
+
+    if type == 'layer':
+        obj_groups = GroupProfile.objects.filter(
+            group__id__in=Layer.objects.values_list('group__id', flat=True)
+        )
+    if type == 'map':
+        obj_groups = GroupProfile.objects.filter(
+            group__id__in=Map.objects.values_list('group__id', flat=True)
+        )
+    if type == 'document':
+        obj_groups = GroupProfile.objects.filter(
+            group__id__in=Document.objects.values_list('group__id', flat=True)
+        )
+    groups = obj_groups.intersection(user_groups).values_list('slug', flat=True)
 
     filter = ""
     for group in groups:
